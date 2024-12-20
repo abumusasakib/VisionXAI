3da3049fea11651e5ae21a3db84ca5cdb9c3166c
Abu Musa Sakib <sakib3963@gmail.com>
2024-12-20 18:37:44 +0600

    chore: reorganize code for readability in mobile app

--- Frontend UI/vision_xai/lib/home/home_screen.dart
+++ Frontend UI/vision_xai/lib/home/home_screen.dart
@@ -5,6 +5,7 @@ import 'package:hive_flutter/hive_flutter.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:vision_xai/constants/ipDetails.dart';
 import 'dart:io';
+import 'package:flutter/foundation.dart'; // Import for kIsWeb
 
 import 'package:vision_xai/home/home_cubit.dart';
 import 'package:vision_xai/home/home_state.dart';
@@ -14,11 +15,11 @@ import 'package:vision_xai/settings/settings_cubit.dart';
 import 'package:vision_xai/settings/settings_state.dart';
 
 Future<String> _getBaseUrl() async {
-  var box = await Hive.box('settings');
+  var box = Hive.box('settings');
 
   // Default values
-  final defaultIp = IPDetails.defaultIP;
-  final defaultPort = IPDetails.defaultPort;
+  const defaultIp = IPDetails.defaultIP;
+  const defaultPort = IPDetails.defaultPort;
 
   // Get IP and Port from Hive, fallback to defaults if not set
   final ip = box.get("ip", defaultValue: defaultIp);
@@ -46,81 +47,91 @@ class Home extends StatelessWidget {
           ),
         ],
       ),
-      body: BlocListener<SettingsCubit, SettingsState>(
-        listener: (context, state) {
-          debugPrint(
-              'BlocListener detected state change: ${state.currentLocale.languageCode}');
-        },
-        child: BlocListener<HomeCubit, HomeState>(
-          listener: (context, state) {
-            if (state.errorMessage != null) {
-              // Show error dialog
-              showDialog(
-                context: context,
-                builder: (context) {
-                  return AlertDialog(
-                    title: Text(context.tr.errorTitle),
-                    content: Text(state.errorMessage!),
-                    actions: [
-                      TextButton(
-                        onPressed: () {
-                          context.read<HomeCubit>().reset();
-                          Navigator.of(context).pop();
-                        },
-                        child: Text(context.tr.ok),
-                      ),
-                    ],
-                  );
-                },
-              );
-            }
-          },
-          child: BlocBuilder<HomeCubit, HomeState>(
-            builder: (context, state) {
-              final cubit = context.read<HomeCubit>();
-              final picker = ImagePicker();
-
-              return LayoutBuilder(
-                builder: (context, constraints) {
-                  final isWideScreen = constraints.maxWidth > 600;
-
-                  return SafeArea(
-                    child: SingleChildScrollView(
-                      child: Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: isWideScreen
-                            ? Row(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Expanded(
-                                    flex: 3,
-                                    child: _buildImageDisplay(context, state),
-                                  ),
-                                  const SizedBox(width: 16),
-                                  Expanded(
-                                    flex: 2,
-                                    child: _buildControls(
-                                        context, cubit, state, picker),
-                                  ),
-                                ],
-                              )
-                            : Column(
-                                mainAxisSize: MainAxisSize
-                                    .min, // Ensuring Column doesn't expand indefinitely
-                                crossAxisAlignment: CrossAxisAlignment.stretch,
-                                children: [
-                                  _buildImageDisplay(context, state),
-                                  const SizedBox(height: 16),
-                                  _buildControls(context, cubit, state, picker),
-                                ],
-                              ),
-                      ),
-                    ),
-                  );
-                },
-              );
+      body: MultiBlocListener(
+        listeners: [
+          BlocListener<SettingsCubit, SettingsState>(
+            listener: (context, state) {
+              debugPrint(
+                  'BlocListener detected state change: ${state.currentLocale.languageCode}');
             },
           ),
+          BlocListener<HomeCubit, HomeState>(
+            listener: (context, state) {
+              if (state.errorMessage != null) {
+                // Show error dialog
+                showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      title: Text(context.tr.errorTitle),
+                      content: Text(state.errorMessage!),
+                      actions: [
+                        TextButton(
+                          onPressed: () {
+                            context.read<HomeCubit>().reset();
+                            Navigator.of(context).pop();
+                          },
+                          child: Text(context.tr.ok),
+                        ),
+                      ],
+                    );
+                  },
+                );
+              }
+            },
+          ),
+        ],
+        child: BlocConsumer<HomeCubit, HomeState>(
+          listener: (context, state) {
+            if (state.errorMessage != null) {
+              // Handle errors
+              debugPrint('Error: ${state.errorMessage}');
+            }
+          },
+          builder: (context, state) {
+            final cubit = context.read<HomeCubit>();
+            final picker = ImagePicker();
+
+            return LayoutBuilder(
+              builder: (context, constraints) {
+                final isWideScreen = constraints.maxWidth > 600;
+
+                return SafeArea(
+                  child: SingleChildScrollView(
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: isWideScreen
+                          ? Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Expanded(
+                                  flex: 3,
+                                  child: _buildImageDisplay(context, state),
+                                ),
+                                const SizedBox(width: 16),
+                                Expanded(
+                                  flex: 2,
+                                  child: _buildControls(
+                                      context, cubit, state, picker),
+                                ),
+                              ],
+                            )
+                          : Column(
+                              mainAxisSize: MainAxisSize
+                                  .min, // Ensuring Column doesn't expand indefinitely
+                              crossAxisAlignment: CrossAxisAlignment.stretch,
+                              children: [
+                                _buildImageDisplay(context, state),
+                                const SizedBox(height: 16),
+                                _buildControls(context, cubit, state, picker),
+                              ],
+                            ),
+                    ),
+                  ),
+                );
+              },
+            );
+          },
         ),
       ),
     );
@@ -165,18 +176,19 @@ class Home extends StatelessWidget {
           label: Text(context.tr.selectImageFromGallery),
         ),
         const SizedBox(height: 16),
-        ElevatedButton.icon(
-          onPressed: () async {
-            final pickedImage = await picker.pickImage(
-              source: ImageSource.camera,
-            );
-            if (pickedImage != null) {
-              cubit.selectImage(File(pickedImage.path));
-            }
-          },
-          icon: const Icon(Icons.camera_alt),
-          label: Text(context.tr.camera),
-        ),
+        if (!kIsWeb && !Platform.isWindows)
+          ElevatedButton.icon(
+            onPressed: () async {
+              final pickedImage = await picker.pickImage(
+                source: ImageSource.camera,
+              );
+              if (pickedImage != null) {
+                cubit.selectImage(File(pickedImage.path));
+              }
+            },
+            icon: const Icon(Icons.camera_alt),
+            label: Text(context.tr.camera),
+          ),
         const SizedBox(height: 16),
         // Button to Upload Image
         ElevatedButton.icon(
@@ -201,14 +213,14 @@ class Home extends StatelessWidget {
               cubit.stopCaptionGeneration();
             },
             icon: const Icon(Icons.stop),
-            label: const Text('Stop'),
+            label: Text(context.tr.stopCaptionGeneration),
           ),
         const SizedBox(height: 16),
         if (state.isLoading)
           Center(
             child: Text(
               context.tr.generatingCaption,
-              style: TextStyle(fontSize: 16, fontStyle: FontStyle.italic),
+              style: const TextStyle(fontSize: 16, fontStyle: FontStyle.italic),
             ),
           ),
         const SizedBox(height: 16),
@@ -226,7 +238,7 @@ class Home extends StatelessWidget {
                 : Text(
                     context.tr.captionText,
                     textAlign: TextAlign.center,
-                    style: TextStyle(fontSize: 18, color: Colors.grey),
+                    style: const TextStyle(fontSize: 18, color: Colors.grey),
                   ),
           ),
         ),
